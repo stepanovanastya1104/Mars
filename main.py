df7ff0b6f2ae5a152df3f06c0d4f0b159df72aa7
Nastya Stepanova <Nastya.stepanova1104@yandex.ru>
2023-02-25 14:36:30 +0300

    1 and 2

---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+from forms.user import RegisterForm
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/mars_explorer.db")
+    app.run()
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    return render_template("index.html", jobs=jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User()
+        user.surname = form.surname.data
+        user.name = form.name.data
+        user.email = form.email.data
+        user.age = int(form.age.data)
+        user.position = form.position.data
+        user.speciality = form.speciality.data
+        user.address = form.address.data
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Страница')
+
+
+if __name__ == '__main__':
+    main()
